07708d8d883ba08c040adaf35a0403b0fbb92510
Bhuvaneshwary <bhuvana.muniganti@gmail.com>
2026-02-09 23:59:35 +0530

    fix: match Netlify env REACT_APP_API_URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@
 
 const API_BASE =
   process.env.NODE_ENV === "production"
-    ? process.env.REACT_APP_API_BASE
+    ? process.env.REACT_APP_API_URL
     : "http://localhost:5000";
 
 export { API_BASE };
